029218487055e30b83c72483970e6c167e124d28
Nikola <dzonza996@gmail.com>
2025-12-08 21:50:03 +0100

    added logic for project section

---
+++ src/components/Projects/Project.tsx
@@ -0,0 +1,117 @@
+import { animate, motion, useMotionValue, useTransform } from 'motion/react';
+import { useState, type FC } from 'react';
+import { Autoplay } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+
+interface LangData {
+  url: string;
+  title: string;
+}
+
+interface ProjectData {
+  title: string;
+  images: string[];
+  logo: string;
+  languages: LangData[];
+}
+
+const Project: FC<ProjectData> = ({ title, images, logo, languages }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+
+  const smoothX = useTransform(x, (v) => v);
+  const smoothY = useTransform(y, (v) => v);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    const rect = e.currentTarget.getBoundingClientRect();
+
+    const mouseX = e.clientX - rect.left;
+    const mouseY = e.clientY - rect.top;
+
+    animate(x, mouseX, { duration: 0.2 });
+    animate(y, mouseY, { duration: 0.2 });
+  };
+
+  return (
+    <div className="relative w-full">
+      <motion.div
+        animate={{
+          scale: isHovered ? 1 : 0,
+          opacity: isHovered ? 1 : 0,
+        }}
+        transition={{ duration: 0.3 }}
+        className="p-3 absolute w-[350px] h-[250px] shadow-[0_15px_45px_rgba(0,0,0,0.25)] bg-[#fbfbfb] z-20 pointer-events-none rounded-[5px] "
+        style={{
+          x: smoothX,
+          y: smoothY,
+          translateX: '-40%',
+          translateY: '-110%',
+          transformOrigin: 'center',
+        }}
+      >
+        {isHovered && (
+          <>
+            <Swiper
+              spaceBetween={50}
+              slidesPerView={'auto'}
+              autoplay={{
+                delay: 2500,
+              }}
+              loop={true}
+              className="w-full h-full rounded-t-[5px]"
+              modules={[Autoplay]}
+              speed={700}
+            >
+              {images.map((url: string, index: number) => (
+                <SwiperSlide key={index}>
+                  <img src={url} alt={title} />
+                </SwiperSlide>
+              ))}
+            </Swiper>
+            <div className="flex justify-center gap-3 absolute top-0 -translate-y-full left-1/2 -translate-x-1/2">
+              {languages.map(({ url, title }, index) => (
+                <img key={index} src={url} alt={title} className="w-7" />
+              ))}
+            </div>
+          </>
+        )}
+      </motion.div>
+      <motion.article
+        initial={{ width: '100%' }}
+        whileHover={{ width: '90%', margin: '0 auto' }}
+        transition={{ duration: 0.3 }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={() => {
+          setIsHovered(false);
+          animate(x, 600, { duration: 0.3 });
+          animate(y, 50, { duration: 0.3 });
+        }}
+        className="w-full flex justify-between h-24 items-center overflow-hidden  rounded-[8px] cursor-pointer relative"
+      >
+        <motion.div
+          animate={{
+            background: isHovered
+              ? 'linear-gradient(to right, #151419 60%, transparent)'
+              : 'linear-gradient(to right, #151419 80%, transparent)',
+          }}
+          transition={{ duration: 0.4 }}
+          className="absolute w-full h-full z-10"
+        ></motion.div>
+        <p className="text-[#fbfbfb] pl-8 text-[25px] z-10">{title}</p>
+        <motion.img
+          className={` h-full object-cover duration-300 ${
+            isHovered ? 'scale-100' : 'scale-150'
+          } `}
+          src={logo}
+          alt={title}
+        />
+      </motion.article>
+    </div>
+  );
+};
+
+export default Project;
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,31 +1,89 @@
-import { motion } from 'motion/react';
 import SectionContainer from '../../ReusableComponents/SectionContainer';
+import Project from './Project';
+
+const vjnData = [
+  '/images/vjn-home.png',
+  '/images/vjn-service.png',
+  '/images/vjn-contact.png',
+];
+const vjnLangImages = [
+  {
+    url: '/images/vue.png',
+    title: 'Vue',
+  },
+  {
+    url: '/images/js-2.png',
+    title: 'JavaScript',
+  },
+  {
+    url: '/images/tailwind.png',
+    title: 'Tailwind',
+  },
+];
+const infiniteLoopData = [
+  '/images/infinite-loop-home.png',
+  '/images/infinite-loop-courses.png',
+  '/images/infinite-loop-login.png',
+];
+
+const infiniteLoopLangImages = [
+  {
+    url: '/images/react.png',
+    title: 'React',
+  },
+  {
+    url: '/images/js-2.png',
+    title: 'JavaScript',
+  },
+  {
+    url: '/images/sass.png',
+    title: 'Sass',
+  },
+];
+
+const grillPopData = [
+  '/images/grillpop-home.png',
+  '/images/grillpop-order.png',
+  '/images/grillpop-about.png',
+];
+
+const grillPopLangImages = [
+  {
+    url: '/images/react.png',
+    title: 'React',
+  },
+  {
+    url: '/images/ts.png',
+    title: 'TypeScript',
+  },
+  {
+    url: '/images/tailwind.png',
+    title: 'Tailwind',
+  },
+];
 const Projects = () => {
   return (
     <SectionContainer title="Projects">
-      <section className="flex flex-col gap-10 pt-10">
-        <motion.article
-          whileHover={{ x: 20 }}
-          transition={{ duration: 0.2 }}
-          className="w-full flex justify-between h-24 items-center overflow-hidden  rounded-[8px] cursor-pointer relative"
-        >
-          <motion.div
-            initial={{
-              background: 'linear-gradient(to right, #151419 70%, transparent)',
-            }}
-            whileHover={{
-              background: 'linear-gradient(to right, #151419 45%, transparent)',
-            }}
-            transition={{ duration: 0.4 }}
-            className=" absolute w-full h-full"
-          ></motion.div>
-          <p className="text-[#fbfbfb] pl-8 text-[25px] z-10">Infinite loop</p>
-          <img
-            className="w-1/2 h-full object-cover"
-            src="/images/test.jpg"
-            alt=""
-          />
-        </motion.article>
+      <section className="flex flex-col  gap-5 pt-5">
+        <Project
+          title="VJN Solutions"
+          logo="/images/vjn-logo.png"
+          images={vjnData}
+          languages={vjnLangImages}
+        />
+        <Project
+          title="Infinite Loop"
+          logo="/images/infinite-loop-logo.png"
+          images={infiniteLoopData}
+          languages={infiniteLoopLangImages}
+        />
+        <Project
+          title="Grill Pop"
+          logo="/images/grillpop-logo.png"
+          images={grillPopData}
+          languages={grillPopLangImages}
+        />
+        {/* <Project /> */}
       </section>
     </SectionContainer>
   );
